3b86c271b46c38f2b6f3787627c8623995a8adfb
boanders74 <83360732+boanders74@users.noreply.github.com>
2021-06-17 11:43:27 -0500

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,7 +9,7 @@ async function run() {
     const token = core.getInput("repo-token");
 
     const octokit = new github.getOctokit(token);
-    console.log(`octokit= ${oktokit}`);
+    console.log(`octokit= ${octokit}`);
     
     const newIssue = await octokit.issues.create({
       repo: github.context.repo.repo,
