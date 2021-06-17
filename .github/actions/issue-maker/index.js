a1a05899f395d0ba4a97e3da5d8219b01df25ba3
boanders74 <83360732+boanders74@users.noreply.github.com>
2021-06-17 12:37:53 -0500

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,7 +7,8 @@ async function run() {
     const issueTitle = core.getInput("issue-title");
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
-
+    console.log(`token= ${token}`);
+    
     const octokit = new github.getOctokit(token);
     console.log(`octokit= ${octokit}`);
     
